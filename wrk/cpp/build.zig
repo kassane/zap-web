5244af5b479e8c4e4c8db82ca567aea1792de4bf
Matheus Catarino <matheus-catarino@hotmail.com>
2024-06-25 13:11:28 -0300

    wrk/cpp: updated to zig-build v0.13.0

--- wrk/cpp/build.zig
+++ wrk/cpp/build.zig
@@ -2,32 +2,43 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
+
+    const lexical = b.dependency("lexical_cast", .{}).path("include");
+    const integer = b.dependency("integer", .{}).path("include");
+    const libboost_beast_dep = b.dependency("beast", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    // also includes another libraries dependencies
+    const libboost_beast = libboost_beast_dep.artifact("beast");
 
     const exe = b.addExecutable(.{
         .name = "cpp-beast",
         .target = target,
         .optimize = optimize,
     });
-    exe.addIncludePath(.{ .path = "." });
-    exe.addCSourceFiles(&.{"main.cpp"}, &.{
+    exe.addCSourceFiles(.{ .files = &.{
+        "main.cpp",
+    }, .flags = &.{
         "-Wall",
         "-Wextra",
-        "-Wshadow",
-    });
-    const libasio_dep = b.dependency("beast", .{
-        .target = target,
-        .optimize = optimize,
-    });
-    const libasio = libasio_dep.artifact("beast");
-    for (libasio.include_dirs.items) |include| {
-        exe.include_dirs.append(include) catch {};
+        "-pedantic",
+        "-std=c++23",
+        "-fexperimental-library",
+    } });
+    for (libboost_beast.root_module.include_dirs.items) |include| {
+        if (include == .other_step) continue;
+        exe.addIncludePath(include.path);
     }
-    exe.linkLibrary(libasio);
+    exe.addIncludePath(integer);
+    exe.addIncludePath(lexical);
+    exe.defineCMacro("BOOST_BEAST_USE_STD_STRING_VIEW", "1");
+    exe.defineCMacro("BOOST_ASIO_NO_DEPRECATED", "1");
+    exe.linkLibrary(libboost_beast);
     exe.linkLibCpp();
-
     b.installArtifact(exe);
+
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
